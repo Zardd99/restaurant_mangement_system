b45c201ccb1a138aae174a070093baaca0535d3c
Sakda Chin <lqykim275@gmail.com>
2025-08-21 22:04:28 +0700

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+import localFont from "next/font/local";
+import { ReactNode } from "react";
+
+const ibmPlexSans = localFont({
+  src: [
+    { path: "/fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Medium.ttf", weight: "500", style: "normal" },
+    { path: "/fonts/IBMPlexSans-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal" },
+  ],
+  variable: "--ibm-plex-sans",
+});
+
+const bebasNeue = localFont({
+  src: [
+    { path: "/fonts/BebasNeue-Regular.ttf", weight: "400", style: "normal" },
+  ],
+  variable: "--bebas-neue",
+});
+
+export const metadata: Metadata = {
+  title: "Restaurant",
+  description:
+    "A restaurant software solution for managing orders and reservations",
+};
+
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+  return (
+    <html lang="en">
+      <body
+        className={`${ibmPlexSans.variable} ${bebasNeue.variable} font-ibm-plex-sans antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
